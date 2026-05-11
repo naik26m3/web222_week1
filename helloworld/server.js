3fce96c028220d641c39535cc6d8eea616485077
Tuan Khai Ngo <dannyngo263@gmail.com>
2026-05-11 10:28:34 -0400

    LOL

--- helloworld/server.js
+++ helloworld/server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
 
-    res.json({ course: "WEB322", section: "NEE", task: "In-class Assignment 1" });
+    res.json({ course: "WEB322", student: "Kai Ngo", task: "In-class Assignment 1" });
 
 });
 
